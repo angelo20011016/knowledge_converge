4520e29b95aa249f47d7e0a066da293b1449db72
angelo20011016 <angelocandu1@gmail.com>
2025-09-14 15:57:01 +0800

    docker化

---
+++ frontend/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Default to development mode
+if [ "$ENV_MODE" = "production" ]; then
+  echo "Running in PRODUCTION mode"
+  # Use the production config (no proxy)
+  cp /etc/nginx/nginx.prod.conf /etc/nginx/conf.d/default.conf
+else
+  echo "Running in DEVELOPMENT mode"
+  # Use the development config (with proxy to backend)
+  cp /etc/nginx/nginx.dev.conf /etc/nginx/conf.d/default.conf
+fi
+
+# Start Nginx in the foreground
+nginx -g 'daemon off;'
